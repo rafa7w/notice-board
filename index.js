d0882d5b3e6d3c45f3455ed42ad6cbd8641fd3d3
Rafael William <rafael.fim@hotmail.com>
2020-12-28 06:26:27 -0300

    working in route /new

--- index.js
+++ index.js
@@ -16,11 +16,20 @@ app.get("/all", (req, res) => {
         res.json(JSON.stringify(posts));
 })
 
-app.post("/new", (req, res) => {
+app.post("/new", bodyParser.json(), (req, res) => {
+        let id = generateID();
+        let title = req.body.title;
+        let description = req.body.description;
 
+        posts.push({id, title, description});
+
+        res.send("Post added");
 })
 
 app.listen(3000, () => {
         console.log(`Server running on port ${PORT}...`);
 })
 
+function generateID(){
+        return Math.random().toString(36).substr(2,9);
+}
