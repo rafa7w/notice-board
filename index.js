f57b174904f4fb1b602daf93039bf8260aca4018
Rafael William <rafael.fim@hotmail.com>
2020-12-28 06:57:39 -0300

    API routes

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const PORT = 3000;
 const express = require("express");
-const bodyParser = require("body-parser");
-const posts = require("./model/posts");
+const path = require("path");
+const apiRoute = require("./routes/api");
 
 const app = express();
 
-app.get("/all", (req, res) => {
-        res.json(JSON.stringify(posts.getAll()));
-})
-
-app.post("/new", bodyParser.json(), (req, res) => {
-        let title = req.body.title;
-        let description = req.body.description;
-
-        posts.newPost(title, description);
-
-        res.send("Post added");
-})
+app.use("/api", apiRoute);
 
 app.listen(3000, () => {
         console.log(`Server running on port ${PORT}...`);
